4db0b67ddfc420abff3b38cdcfd8708c6dd134cb
cindykimxp <cindykim100@gmail.com>
2020-12-07 12:39:05 -0800

    Bag of Words
    - bag_of_words python program to find and rank bag of words
    - democrats output file containing word and frequency
    - republican output file containing word and frequency

---
+++ bag_of_words.py
@@ -0,0 +1,51 @@
+def main():
+    democrats_file = open('1.source_preprocess/corpora/party_filtered/democrat/dem_test_lemma.txt', 'r')
+    read_df = democrats_file.readlines()
+    demo_words = {}
+
+    bagofwords_d = open('democrats_bagofwords.txt', 'w')
+    bagofwords_r = open('republicans_bagofwords.txt', 'w')
+    
+    repub_file = open('1.source_preprocess/corpora/party_filtered/republican/rep_test_lemma.txt', 'r')
+    read_rf = repub_file.readlines()
+    repub_words = {}
+    
+    for line in read_df:
+        line = line.strip('\n').split('\t')
+        if (len(line) == 0 or len(line) == 1):
+            continue
+        word = line[0].lower()
+
+        if (word != ""):
+            if word in demo_words:
+                demo_words[word] += 1
+            else:
+                demo_words[word] = 1
+
+    for line in read_rf:
+        line = line.strip('\n').split('\t')
+        if (len(line) == 0 or len(line) > 2):
+            continue
+        word = line[0].lower()
+
+        if (word != ""):
+            if word in repub_words:
+                repub_words[word] += 1
+            else:
+                repub_words[word] = 1
+
+    demo_sorted = sorted(demo_words.items(), key=lambda x: x[1], reverse=True)
+    repub_sorted = sorted(repub_words.items(), key=lambda x: x[1], reverse=True)
+
+    for words in demo_sorted:
+        bagofwords_d.write(words[0] + '\t' + str(words[1]) + '\n')
+    for words in repub_sorted:
+        bagofwords_r.write(words[0] + '\t' + str(words[1]) + '\n')
+
+    democrats_file.close()
+    repub_file.close()
+    bagofwords_d.close()
+    bagofwords_r.close()
+    
+    
+main()
